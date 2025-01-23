5e47fe8623eef65efa08636a0daac7168fca13d7
Scott M Parrish <anithri@gmail.com>
2025-01-22 19:25:59 -0700

    fix button bars

--- app/views/components/btn_bar_component.rb
+++ app/views/components/btn_bar_component.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 class BtnBarComponent < ApplicationComponent
-  def initialize(*args, **kwargs)
+  attr_reader :element
+  def initialize(element = 'ul', *args, **kwargs)
+    @element = element
   end
   def view_template(&content)
-    ul class: "p-0 m-0 inline-flex rounded-lg", &content
+    if element == 'ul'
+      ul class: "p-0 m-0 inline-flex rounded-lg", &content
+    else
+      div class: "p-0 m-0 inline-flex rounded-lg", &content
+    end
   end
 end
